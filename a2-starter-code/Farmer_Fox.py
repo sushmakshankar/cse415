ea30afd4b1117d441dea32746cd686d9e8df9dcb
Sushma Shankar <sshan854@uw.edu>
2025-10-07 18:16:14 -0700

    starter information and data

--- a2-starter-code/Farmer_Fox.py
+++ a2-starter-code/Farmer_Fox.py
@@ -18,7 +18,23 @@ the Farmer, Fox, Chicken, and Grain.
 
 # Put your metadata here, in the same format as in HumansRobotsFerry.
 
+#</METADATA>
+PROBLEM_NAME = "Farmer, Fox, Chicken, and Grain"
+PROBLEM_VERSION = "1.1"
+PROBLEM_AUTHORS = ['S. Shankar, D. Modi']
+PROBLEM_CREATION_DATE = "10-OCT-2025"
+
+
 # Start your Common Code section here.
+
+#<COMMON_CODE>
+# change this potentially? what is common code
+Farmer = 1
+Chicken = 1
+Fox = 1
+Grain = 1
+LEFT=4 
+RIGHT=0
 
 class State:
 
