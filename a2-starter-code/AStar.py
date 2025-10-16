bda0cd1385c6821fbd461775081c070a77d92bef
Sushma Shankar <sshan854@uw.edu>
2025-10-16 15:43:02 -0700

    i think i recreated ucs instead of a star

--- a2-starter-code/AStar.py
+++ a2-starter-code/AStar.py
@@ -79,53 +79,87 @@ class AStar:
         # STEP 1b. Assign g=0 to the start state.
         self.g[initial_state] = 0.0
 
-        while True:
-            if len(self.OPEN) == 0:
-                print("Failure: No path found")
-                return
-
+        while len(self.OPEN) > 0:
+            # print("Failure: No path found")
+            # return
             if self.VERBOSE:
-                print(f"\n{self.COUNT} states expanded so far.")
-                print(f"Length of OPEN: {len(self.OPEN)}")
-
+                # print(f"\n{self.COUNT} states expanded so far.")
+                # print(f"Length of OPEN: {len(self.OPEN)}")
+                report(self.OPEN, self.CLOSED, self.COUNT)
             if len(self.OPEN) > self.MAX_OPEN_LENGTH:
                 self.MAX_OPEN_LENGTH = len(self.OPEN)
 
             # STEP 2. Take the node off OPEN with the lowest f(n)=g(n)+h(n)
             (S, P) = self.OPEN.delete_min()
             self.CLOSED.append(S)
+
+
             if S.is_goal():
-                if self.VERBOSE:
-                    print("\nGoal reached!")
-                self.PATH = []
-                self.PATH_LENGTH = 0
-                self.TOTAL_COST = self.g[n]
-                while S is not None:
-                    self.PATH.append(n)
-                    S = self.BACKLINKS[n]
-                    self.PATH_LENGTH += 1
-                self.PATH.reverse()
-                print("The path to the goal is:")
-                for n in self.PATH:
-                    print(n)
-                print(f"Length of this path: {self.PATH_LENGTH} states")
-                print(f"Total cost of this path: {self.TOTAL_COST}")
-                print(self.Problem.goal_message(self.PATH[-1]))
+                print(self.Problem.GOAL_MESSAGE_FUNCTION(S))
+                self.PATH = [str(state) for state in self.backtrace(S)]
+                self.PATH_LENGTH = len(self.PATH) - 1
+                print(f'Length of solution path found: {self.PATH_LENGTH} edges')
+                self.TOTAL_COST = self.g[S]
+                print(f'Total cost of solution path found: {self.TOTAL_COST}')
                 return
+            self.COUNT += 1
 
             # STEP 3. Add n to CLOSED
             self.CLOSED.append(S)
 
             # STEP 4. Expand node S, generating each of its successors S'
+            gs = self.g[S]  # Save the cost of getting to S in a variable.
             for op in self.Problem.OPERATORS:
                 if op.is_applicable(S):
                     S_prime = op.apply(S)
+                    if S_prime in self.CLOSED:
+                        # print("Already have this state, in CLOSED. del ...")
+                        del S_prime
+                        continue
                     cost_S_to_Sprime = S.edge_distance(S_prime)
-                    if S_prime not in self.g:
-                        # This is the first time we've reached S'
-                        self.g[S_prime] = float
+                    new_g = gs + cost_S_to_Sprime
+
+                    if S_prime in self.OPEN:
+                        # print("new_state is in OPEN already, so...")
+                        P = self.OPEN[S_prime]
+                        if new_g < P:
+                            # print("New priority value is lower, so del older one")
+                            del self.OPEN[S_prime]
+                            self.OPEN.insert(S_prime, new_g)
+                        else:
+                            # print("Older one is better, so del new_state")
+                            del S_prime
+                            continue
+                    else:
+                        # print("new_state was not on OPEN at all, so just put it on.")
+                        self.OPEN.insert(S_prime, new_g)
+                    self.BACKLINKS[S_prime] = S
+                    self.g[S_prime] = new_g
+        
+        return None
+    def backtrace(self, S):
+        path = []
+        while S:
+            path.append(S)
+            S = self.BACKLINKS[S]
+        path.reverse()
+        print("Solution path: ")
+        for s in path:
+            print(s)
+        return path
 
 
+def report(opn, closed, count):
+    """
+    Reports the current statistics:
+    Length of open list
+    Length of closed list
+    Number of states expanded
+    """
+    print(f"len(OPEN)= {len(opn)}", end='; ')
+    print(f"len(CLOSED)= {len(closed)}", end='; ')
+    print(f"COUNT = {count}")
+    
 if __name__ == '__main__':
     if sys.argv == [''] or len(sys.argv) < 2:
         Problem = "FranceWithDXHeuristic"
